d77aad5d8db8d8fb59d71a77cf7e5c190ef2dba2
nciampelletti <natashat@shaw.ca>
2022-07-12 23:13:52 -0700

    setup task Crud in controllers

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,19 +1,58 @@
 const Task = require("../models/taskModel")
 
 exports.createTask = async (req, res) => {
-  const task = await Task.create({
-    name: req.body.name,
-    completed: req.body.completed,
-  })
-  res.status(201).json({ task })
+  try {
+    const task = await Task.create(req.body)
+
+    res.status(201).json({
+      status: "success",
+      data: { task },
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    })
+  }
 }
 
 exports.getTasks = async (req, res) => {
-  res.send(" get Tasks")
+  try {
+    const tasks = await Task.find()
+
+    res.status(200).json({
+      status: "success",
+      data: { tasks },
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    })
+  }
 }
 
 exports.getTask = async (req, res) => {
-  res.send("get Task")
+  try {
+    const task = await Task.findOne({ _id: req.params.id })
+
+    if (!task) {
+      return res.status(404).json({
+        status: "fail",
+        message: `id: ${req.params.id} has not been found`,
+      })
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: { task },
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    })
+  }
 }
 
 exports.createTaskPostman = async (req, res) => {
@@ -21,9 +60,53 @@ exports.createTaskPostman = async (req, res) => {
 }
 
 exports.updateTask = async (req, res) => {
-  res.send("update Task")
+  try {
+    const TaskId = req.params.id
+
+    const task = await Task.findOneAndUpdate({ _id: TaskId }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!task) {
+      return res.status(404).json({
+        status: "fail",
+        message: `id: ${TaskId} has not been found`,
+      })
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: task,
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    })
+  }
 }
 
 exports.deleteTask = async (req, res) => {
-  res.send("delete Task")
+  try {
+    const TaskId = req.params.id
+    const task = await Task.findOneAndDelete({ _id: TaskId })
+
+    if (!task) {
+      return res.status(404).json({
+        status: "fail",
+        message: `id: ${TaskId} has not been found`,
+      })
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: null,
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    })
+  }
 }
--- models/taskModel.js
+++ models/taskModel.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    minlength: [3, "can not be less than 3"],
+    maxlength: [20, "can not be more than 20"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 module.exports = mongoose.model("Task", TaskSchema)
